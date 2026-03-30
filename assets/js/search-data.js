491520441e4deb96c239e3dc603cd621fd0c1b89
juliayoussef <107482498+juliayoussef@users.noreply.github.com>
2026-03-30 23:49:32 +0000

    Deploying to gh-pages from @ juliayoussef/juliayoussef.github.io@2021f1e4ffbf51a2908f66c8eccf4da78f01527d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,20 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-essays",
+          title: "essays",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/essays/index.html";
+          },
+        },{id: "nav-notes",
+          title: "notes",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/notes/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of projects.",
