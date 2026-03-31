6f8d5d4eb39d7449f6ee2c21bdbc2450e44575d7
juliayoussef <107482498+juliayoussef@users.noreply.github.com>
2026-03-31 04:25:21 +0000

    Deploying to gh-pages from @ juliayoussef/juliayoussef.github.io@3e9e36309a4a5048a68dff06d790ab56e4545144 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-notes",
           title: "notes",
-          description: "",
+          description: "notes and review content hub",
           section: "Navigation",
           handler: () => {
             window.location.href = "/notes/";
