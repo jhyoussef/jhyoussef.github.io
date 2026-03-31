f0bd1ff8b83860e87a01533c35cc5328aa3bc779
juliayoussef <107482498+juliayoussef@users.noreply.github.com>
2026-03-31 01:18:07 +0000

    Deploying to gh-pages from @ juliayoussef/juliayoussef.github.io@41b5413f6905423a65a92bd4578c5aaeba6686c8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -62,7 +62,12 @@ ninja.data = [{
             window.location.href = "/blog/2025/testing-out-the-format/";
           
         },
-      },{id: "news-my-first-announcement-sparkles-smile",
+      },{id: "art-test-title",
+          title: 'Test Title',
+          description: "Short description",
+          section: "Art",handler: () => {
+              window.location.href = "/art/test-art/";
+            },},{id: "news-my-first-announcement-sparkles-smile",
           title: 'My first announcement! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "projects-learning-anatomy",
